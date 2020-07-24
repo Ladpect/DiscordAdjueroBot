b06f2f8e4f072860da3cff35fec435f33bf662a8
adjuero <61926539+adjuero@users.noreply.github.com>
2020-07-24 10:59:37 +0900

    Update 아드유로.py

--- 아드유로.py
+++ 아드유로.py
@@ -118,7 +118,7 @@ async def 탈퇴(ctx):
 @client.command(pass_context=True)
 async def 광산(ctx):
     cursor = db.cursor()
-    cursor.execute(f"SELECT user_id, user_name, coin, 둘기석, 삼다석, 불즈석, 로아석, 에릭석, 염라석, 템프석, 태양석, 사랑석, 아드석 FROM 광산 WHERE user_id = '{ctx.author.id}'")
+    cursor.execute(f"SELECT user_id, user_name, coin, 둘기석, 삼다석, 불즈석, 로아석, 에릭석, 염라석, 템프석, 태양석, 사랑석, 아드석 FROM 광산 WHERE user_id = {ctx.author.id}")
     result = cursor.fetchone()
     if result is None:
         await ctx.send("`ad가입`을 통해 가입을 해주세요.")
